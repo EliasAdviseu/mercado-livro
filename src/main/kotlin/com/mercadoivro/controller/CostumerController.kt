3299453141210898ac4c0ab3dda8108c016d38bb
elias.oliveira <elias.oliveira@adviseu.com.br>
2023-10-24 14:25:26 -0300

    feat: hello world

---
+++ src/main/kotlin/com/mercadoivro/controller/CostumerController.kt
@@ -0,0 +1,15 @@
+package com.mercadoivro.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("/costumer")
+class CostumerController {
+    @GetMapping("/")
+    fun helloWorld(): String {
+        return "Hello world"
+    }
+}
